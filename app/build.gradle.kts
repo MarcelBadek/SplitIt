56a92ad602f567b0af64eb665d90999d4e91bc4d
Marcel <kriollik@gmail.com>
2023-12-28 19:35:39 +0100

    fix: fix X D

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-auth:22.3.0")
-    implementation("com.google.firebase:firebase-firestore:24.10.0")
     // FirebaseUI for Firebase Realtime Database
     implementation("com.firebaseui:firebase-ui-database:8.0.2")
     // FirebaseUI for Cloud Firestore
