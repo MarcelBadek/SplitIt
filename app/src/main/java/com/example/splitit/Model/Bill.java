0679c867673ba721baaa1ca0a60a1dfda6ad3b4a
Marcel <kriollik@gmail.com>
2023-12-29 19:44:53 +0100

    feat: add setting total cost in group view

--- app/src/main/java/com/example/splitit/Model/Bill.java
+++ app/src/main/java/com/example/splitit/Model/Bill.java
@@ -4,20 +4,31 @@ import java.util.List;
 
 public class Bill {
     private String name;
+    private double price;
     private List<Member> members;
 
     public Bill() {}
 
-    public Bill(String name, List<Member> members) {
+    public Bill(String name, double price, List<Member> members) {
         this.name = name;
+        this.price = price;
         this.members = members;
     }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     public List<Member> getMembers() {
